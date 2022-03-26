91ab6ad5fd86f1236d4d6cad48475b42144717ff
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2022-03-27 01:20:25 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-𝙎𝘾 𝙊𝙍𝙄 𝘽𝙔 𝙇𝙔𝙉𝙓𝙕𝙔
+𝙎𝘾 𝙊𝙍𝙄 𝘽𝙔 𝙇𝙔𝙉𝙓𝙕𝙔 🔥
 github.com/LynnXzy/scbotz-v1
 `.trim(), '©LynXzy🥀', 'Makasih', 'Thanks')
 handler.help = ['sourcecode']
