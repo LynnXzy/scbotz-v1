97e234d4b7395730e105783c3c3c9909f3fe717e
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2022-03-31 22:49:29 +0800

    Update botchat2.js

--- plugins/botchat2.js
+++ plugins/botchat2.js
@@ -3,10 +3,10 @@ let fs = require('fs')
 let handler = async (m, { conn, command, text }) => {
 let img = './src/RadBot.png'
 let user = global.db.data.users[m.sender]
-conn.sendButton(m.chat, `Halo juga kak ${conn.getName(m.sender)}`, 'LynXzy', 'Menu', '#Menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 999, thumbnail: fs.readFileSync('./src/RadBotZ.jpg')
+conn.sendButton(m.chat, `Halo 👋 ${conn.getName(m.sender)}`, 'LynnXzy', 'Menu', '#Menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 999, thumbnail: fs.readFileSync('./src/IMG-20220322-WA0026.jpg')
 }}})
 }
-handler.customPrefix = /^(Hallo|halo|Halo|hallo|hi|helo|hai|hi|Hai|Helo|Hello)$/i
+handler.customPrefix = /^(Hallo|halo|Halo|hallo|hi|helo|hai|hi|Hai|Helo|Hello|P|p)$/i
 handler.command = new RegExp
 
 module.exports = handler
