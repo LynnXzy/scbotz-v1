3cc9e3cbf9ec513a8777071844e8c1162b3f7ea1
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2022-04-03 02:49:06 +0800

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -68,7 +68,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} ⏲️| Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} 👾|Botz by LynXzy💌`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} ⏲️`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
